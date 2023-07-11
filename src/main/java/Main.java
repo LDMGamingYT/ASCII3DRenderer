ff7ac3dda89cf0c3b7398849bc6133efa40c423c
Logan Dhillon <logandhi64@gmail.com>
2023-07-11 00:40:06 -0400

    Changed screen text to fill window (requires testing)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,9 @@ public class Main {
         if (!renderToFile) UI.initialize();
         if (renderToFile) LOG.info("Skipping UI initialization (file render only)");
 
-        Screen screen = new Screen(16, 16);
+        // TODO: 2023-07-11 Make sure this works on all resolutions.
+        //                  last tested with Screen size is 1536x864, setting window size to 768x432
+        Screen screen = new Screen(64, 16);
         for (int x = 0; x < screen.size().width; x++) {
             for (int y = 0; y < screen.size().height; y++) {
                 screen.drawPixel(new Vector2(x, y), new Pixel(1f));
