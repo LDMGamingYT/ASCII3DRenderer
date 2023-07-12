10a00f372e28a62596f097b07937b4933b6e3fb0
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-07-12 15:07:37 -0400

    Created pseudo-code for RayCaster

---
+++ src/main/java/net/ldm/asciirenderer/renderer/RayCaster.java
@@ -0,0 +1,17 @@
+package net.ldm.asciirenderer.renderer;
+
+public class RayCaster {
+	public RayCaster() {
+
+	}
+
+	public void rayCast(int direction) {
+		// Fire single ray from player to wall given a direction and call Renderer to draw the according line.
+		// Use a variable like "renderer x index/position" to keep track of where we are in the screen and what line
+		// to draw to the screen.
+	}
+
+	public void coneCast() {
+		// Fire rays to fill screen according to FOV
+	}
+}
