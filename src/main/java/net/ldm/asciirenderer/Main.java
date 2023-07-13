24dbb0a16c38226659806cd2773605d9cf496a1f
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-07-13 13:08:57 -0400

    Removed TODO

--- src/main/java/net/ldm/asciirenderer/Main.java
+++ src/main/java/net/ldm/asciirenderer/Main.java
@@ -10,15 +10,15 @@ public class Main {
     public static void main(String[] args) {
         UI.initialize();
 
-        // TODO: 2023-07-11 Make sure this works on all resolutions.
-        //                  last tested with Screen size is 1536x864, setting window size to 768x432
         Screen screen = new Screen(136, 32);
 
         new Thread(() -> {
-            try {
-                Renderer.update(screen);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            while (true) {
+                try {
+                    Renderer.update(screen);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }, "Render-Thread").start();
     }
