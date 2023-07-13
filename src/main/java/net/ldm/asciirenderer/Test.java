77fc27577eea22250a5a378a3eedeadd66aa1083
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-07-13 13:31:58 -0400

    Created primitive movement system

--- src/main/java/net/ldm/asciirenderer/Test.java
+++ src/main/java/net/ldm/asciirenderer/Test.java
@@ -1,8 +1,11 @@
 package net.ldm.asciirenderer;
 
 import net.ldm.asciirenderer.game.GameObject;
+import net.ldm.asciirenderer.game.Map;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
+
+import java.util.Scanner;
 
 public class Test {
 	private static final Logger LOG = LoggerContext.getContext().getLogger(Test.class);
@@ -11,5 +14,18 @@ public class Test {
 		LOG.warn("You are currently running a TEST ENVIRONMENT. If this is intended, ignore this message. This class should NOT be included in production.");
 
 		GameObject player = new GameObject();
+		Scanner in = new Scanner(System.in);
+
+		//noinspection InfiniteLoopStatement
+		while (true) {
+			System.out.println(Map.MAP);
+			switch (in.nextLine()) {
+				case "w" -> player.position.move(0, 1);
+				case "a" -> player.position.move(1, 0);
+				case "s" -> player.position.move(0, -1);
+				case "d" -> player.position.move(-1, 0);
+				default -> System.out.println("Invalid input");
+			}
+		}
 	}
 }
